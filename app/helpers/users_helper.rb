dd15582447469905dbd62f5332e1a10b50492698
yukiabineko <Yuki14070912>
2019-11-19 10:26:26 +0900

    保護者public falseでレコード１のみview対処

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -10,5 +10,29 @@ module UsersHelper
       end    
     end    
     return count
-  end      
+  end 
+#保護者の場合、レコードが一の場合でpublic falseの場合th非表示
+  def public_check
+    result = false
+    if !@user.id == 1 && @user.documents.all.count == 1
+      document = @user.documents.all.last
+      if document.public == false
+        result = false
+      end
+    #複数レコードがある場合public trueが一つでもあればtrue  
+    elsif @user.documents.all.count > 1 
+      count = 0
+      documents = @user.documents.all
+      documents.each do |document|
+        if document.public == true
+          count += 1
+        end  
+        if count > 0
+          result = true
+        end  
+      end 
+    end  
+    return result
+  end
+       
 end
